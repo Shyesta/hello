c3d7c9970ff277a24d0c2fad6ca65a4a13ca4d5e
Sage Bain <bain.sage@student.greenriver.edu>
2024-01-11 13:54:15 -0500

    Created Fat Free Framework for project

--- index.php
+++ index.php
@@ -1,14 +1,20 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Hello</title>
-</head>
-<body>
-    <h1>Hello!</h1>
-</body>
-</html>
 <?php
+    // This is my CONTROLLER!
+
+    // Turn on error reporting
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
+
+    // Require the autoload file
+    require_once('vendor/autoload.php');
+
+    // Instantiate the F3 Base class
+    $f3 = Base::instance();
+
+    // Define a default route
+    $f3->route('GET /', function()
+    {
+        echo "<h1>Hello!</h1>";
+    });
+
+    $f3->run();
